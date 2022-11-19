5c209d6581b8371763a5e961ca6d4620dd3af0a1
frustak <soulburn0017@gmail.com>
2022-11-20 00:29:26 +0330

    save downloaded website to file

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
 use anyhow::Result;
 use dotenvy::dotenv;
 use std::{
-    collections::{HashMap, VecDeque},
+    collections::VecDeque,
     env,
+    fs::{self, File, OpenOptions},
+    io::Write,
+    path::Path,
 };
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
     let seed = env::var("SEED").expect("SEED environment variable must be set");
-    let queue = VecDeque::from([seed]);
-    let resp = reqwest::get("https://dummyjson.com/products")
-        .await?
-        .text()
-        .await?;
-    println!("{:?}", resp);
+    let mut que = VecDeque::from([seed]);
+    let url = que.pop_front().unwrap();
+    let resp = reqwest::get(url).await?.text().await?;
+    let data_dir = Path::new("./data");
+    if !data_dir.exists() {
+        fs::create_dir(data_dir)?;
+    }
+    let repo_path = data_dir.join("repo.txt");
+    let mut repo = if !repo_path.exists() {
+        File::create(repo_path)?
+    } else {
+        OpenOptions::new().write(true).open(repo_path)?
+    };
+    repo.write_all(resp.as_bytes())?;
     Ok(())
 }
