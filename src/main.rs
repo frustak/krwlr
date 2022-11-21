71af70bcb8adb050f097c2777c09c76a93130710
frustak <soulburn0017@gmail.com>
2022-11-21 14:06:30 +0330

    refactor and add log

--- src/main.rs
+++ src/main.rs
@@ -4,40 +4,56 @@ use select::{document::Document, predicate::Name};
 use std::{
     collections::{HashSet, VecDeque},
     env,
-    fs::{self, OpenOptions},
+    fs::{self, File, OpenOptions},
     io::Write,
     path::Path,
 };
+use tracing::info;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .pretty()
+        .without_time()
+        .compact()
+        .with_file(false)
+        .with_line_number(false)
+        .with_target(false)
+        .init();
     dotenv().ok();
-    let seed = env::var("SEED").expect("SEED environment variable must be set");
+    let seed = env::var("SEED").expect("SEED environment variable is missing!");
     let mut que = VecDeque::from([seed.clone()]);
-    let data_dir = Path::new("./data");
-    if !data_dir.exists() {
-        fs::create_dir(data_dir)?;
+    let mut uniq_links: HashSet<String> = HashSet::from([seed]);
+    let mut repo = open_repo()?;
+    info!("Starting the crawl");
+    while !que.is_empty() {
+        let url = que.pop_front().unwrap();
+        info!(url);
+        let resp = reqwest::get(url).await?.text().await?;
+        repo.write_all(resp.as_bytes())?;
+        let new_links: HashSet<String> = Document::from(resp.as_str())
+            .find(Name("a"))
+            .filter_map(|node| node.attr("href"))
+            .map(|link| link.to_string())
+            .collect::<HashSet<String>>()
+            .difference(&uniq_links)
+            .cloned()
+            .collect();
+        uniq_links.extend(new_links.clone());
+        new_links.into_iter().for_each(|link| que.push_back(link));
     }
+    info!("Crawl complete");
+    Ok(())
+}
+
+fn open_repo() -> Result<File> {
+    let data_dir = Path::new("./data");
+    fs::create_dir_all(data_dir)?;
     let repo_path = data_dir.join("repo.txt");
-    let mut repo = OpenOptions::new()
+    let repo = OpenOptions::new()
         .write(true)
         .create(true)
         .truncate(true)
         .open(repo_path)?;
-    let mut uniq_links: HashSet<String> = HashSet::from([seed]);
-    while !que.is_empty() {
-        let url = que.pop_front().unwrap();
-        println!("Crawling {}", url);
-        let resp = reqwest::get(url).await?.text().await?;
-        repo.write_all(resp.as_bytes())?;
-        let links: HashSet<String> = Document::from(resp.as_str())
-            .find(Name("a"))
-            .filter_map(|node| node.attr("href"))
-            .map(|link| link.to_string())
-            .collect();
-        let new_links: HashSet<String> = links.difference(&uniq_links).cloned().collect();
-        uniq_links.extend(new_links.clone());
-        que.extend(new_links);
-    }
-    Ok(())
+    Ok(repo)
 }
